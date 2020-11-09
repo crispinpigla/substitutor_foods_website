3dbd68cf4a674b944b38da1730615dab918e851b
Crispin Pigla <crispinpigla@yahoo.fr>
2020-11-09 16:18:18 +0100

    + before deploiement 17

--- pure_beurre_django/pure_beurre_django/settings.py
+++ pure_beurre_django/pure_beurre_django/settings.py
@@ -110,9 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "fr"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Paris"
 
 USE_I18N = True
 
@@ -150,3 +150,4 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'wt/static/media/')
