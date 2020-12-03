e13b656102f0aa13cd238183fbc23e39063a9759
Crispin Pigla <crispinpigla@yahoo.fr>
2020-12-03 17:40:31 +0100

    + travis 65

--- pure_beurre_django/settings.py
+++ pure_beurre_django/settings.py
@@ -44,7 +44,7 @@ if os.environ.get("ENV") == "PRODUCTION":
     # If you wish to associate users to errors (assuming you are using
     # django.contrib.auth) you may enable sending PII data.
     #send_default_pii=True
-)
+#)
 else:
     DEBUG = True
     ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
