34cc6f046677126cadf31d81316b66d2434ad7cf
Crispin Pigla <crispinpigla@yahoo.fr>
2020-12-04 00:56:19 +0100

    + travis 94

--- pure_beurre_django/travis_settings.py
+++ pure_beurre_django/travis_settings.py
@@ -4,9 +4,15 @@ import os
 
 SECRET_KEY = os.environ.get('SECRET_KEY', "y2!bd%d^@_)*e77o&gvgy(bndcj))ym*l_6waq%ig#!a3m6r0u")
 
-INSTALLED_APPS = (
-    'django_secrets',
-)
+INSTALLED_APPS = [
+    "substitutor.apps.SubstitutorConfig",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+]
 
 DATABASES = {
     'default': {
