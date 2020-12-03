a153d166316f95d200666fd95a32cb9b507cf577
Crispin Pigla <crispinpigla@yahoo.fr>
2020-12-03 17:43:14 +0100

    + travis 66

--- pure_beurre_django/settings.py
+++ pure_beurre_django/settings.py
@@ -18,7 +18,7 @@ import os
 import dj_database_url
 
 import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
+#from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
