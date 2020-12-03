2e9e7783efbb94b80ad0fb27d0f97658d46fdfe2
Crispin Pigla <crispinpigla@yahoo.fr>
2020-12-04 00:28:11 +0100

    + travis 89

--- pure_beurre_django/travis_settings.py
+++ pure_beurre_django/travis_settings.py
@@ -4,6 +4,10 @@ import os
 
 SECRET_KEY = os.environ.get('SECRET_KEY', "y2!bd%d^@_)*e77o&gvgy(bndcj))ym*l_6waq%ig#!a3m6r0u")
 
+INSTALLED_APPS = (
+    'django_secrets',
+)
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
