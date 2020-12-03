185b5ddbc0f37425756446eeef7b0c7de5ff1dd8
Crispin Pigla <crispinpigla@yahoo.fr>
2020-12-03 02:57:31 +0100

    + travis 14 + sentry 1

--- substitutor/urls.py
+++ substitutor/urls.py
@@ -4,9 +4,14 @@ from django.urls import path
 
 from . import views  # import views so we can use them in urls.
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
 
 urlpatterns = [
     path("", views.redirect_home),
+    path('sentry-debug/', trigger_error),
     url("home/", views.home),
     url("substitute/", views.substitute),
     url(r"^(?P<product_id>[0-9]+)/$", views.detail),
