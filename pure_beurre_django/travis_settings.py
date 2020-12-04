b08a3abaea8e9c04b91e13d77aa0c70ebda3c9b2
Crispin Pigla <crispinpigla@yahoo.fr>
2020-12-04 01:58:48 +0100

    + travis 102

--- pure_beurre_django/travis_settings.py
+++ pure_beurre_django/travis_settings.py
@@ -52,17 +52,4 @@ DATABASES = {
 }
 
 
-# Static files settings
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_ROOT = os.path.join(PROJECT_ROOT, "staticfiles")
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, "static"),)
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES["default"].update(db_from_env)
-
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'wt/static/media/')
+STATIC_URL = "/static/"
