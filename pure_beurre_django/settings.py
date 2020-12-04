da1d3ec335f64c28eb64e75c2b83dcc562515d6b
Crispin Pigla <crispinpigla@yahoo.fr>
2020-12-04 01:11:51 +0100

    + travis 97

--- pure_beurre_django/settings.py
+++ pure_beurre_django/settings.py
@@ -18,7 +18,7 @@ import os
 import dj_database_url
 
 import sentry_sdk
-#from sentry_sdk.integrations.django import DjangoIntegration
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -36,15 +36,15 @@ if os.environ.get("ENV") == "PRODUCTION":
     DEBUG = False
     ALLOWED_HOSTS = ["purebeurre0.herokuapp.com", "34.105.133.157"]
     
-    #sentry_sdk.init(
-    #dsn="https://924a79e00ceb4586bcbdedfd1b3feced@o486074.ingest.sentry.io/5542306",
-    #integrations=[DjangoIntegration()],
-    #traces_sample_rate=1.0,
+    sentry_sdk.init(
+    dsn="https://924a79e00ceb4586bcbdedfd1b3feced@o486074.ingest.sentry.io/5542306",
+    integrations=[DjangoIntegration()],
+    traces_sample_rate=1.0,
 
     # If you wish to associate users to errors (assuming you are using
     # django.contrib.auth) you may enable sending PII data.
-    #send_default_pii=True
-#)
+    send_default_pii=True
+)
 else:
     DEBUG = True
     ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
