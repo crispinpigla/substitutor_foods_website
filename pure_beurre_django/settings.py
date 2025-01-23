f08871cf0b625d82524a57d208fb182028b7b9d6
Crispin Pigla <crispinpigla@yahoo.fr>
2025-01-23 12:17:48 +0100

    dj_database not even used in settings

--- pure_beurre_django/settings.py
+++ pure_beurre_django/settings.py
@@ -158,7 +158,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, "staticfiles")
 STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, "static"),)
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES["default"].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES["default"].update(db_from_env)  # for Heroku
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'wt/static/media/')
